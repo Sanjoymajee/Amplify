65a212b92f774e38b08167dd26a06dbb4573da40
Sanjoymajee <sanjoymajee02@gmail.com>
2023-03-22 03:05:57 +0530

    Error message fixed

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const app = express();
+const flash = require('connect-flash');
 require("dotenv").config();
+const path = require('path');
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -13,11 +16,14 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
 });
-
-const app = express();
+const ejs = require('ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(flash());
 
 // Set up session middleware
 app.use(
@@ -35,18 +41,38 @@ app.use(passport.session());
 
 // Set up the local strategy for username and password authentication
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
+passport.use('login',new LocalStrategy(async (username, password, done) => {
     try {
       const user = await User.findOne({ username });
       if (!user) {
-        return done(null, false, { message: "Incorrect username." });
+        return done(null, false, { message: "Incorrect username" });
       }
       const passwordMatch = bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        return done(null, false, { message: "Incorrect password." });
+      if (passwordMatch) {
+        return done(null, false, { message: "Incorrect password" });
       }
       return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.use('signup', new LocalStrategy(async (username, password, done) => {
+  try {
+      const user = await User.findOne({ username });
+      if (user) {
+          return done(null, false, { message: 'Username already taken' });
+      }
+      const newUser = new User({ username, password });
+      newUser
+        .save()
+        .then((user) => {
+          req.login(user, (err) => {
+            if (err) console.log(err);
+            res.redirect("/");
+          });
+        })
     } catch (err) {
       return done(err);
     }
@@ -61,57 +87,72 @@ function ensureAuthenticated(req, res, next) {
   res.redirect("/login");
 }
 
+// chechUsername middleware
+// function checkUsername(req, res, next) {
+//   const { username } = req.body;
+
+//   User.findOne({ username }, (err, user) => {
+//     if (err) return next(err);
+//     if (user) {
+//       req.flash('error', 'Username is already taken');
+//       return res.redirect('/login');
+//     }
+//     next();
+//   });
+// }
+
 
 // Set up routes
-app.get("/", (req, res) => {
-  res.send(`
-    <form method="POST" action="/logout">
-      <button type="submit">Log out</button>
-    </form>
-  `);
-});
-
-app.get("/dashboard",ensureAuthenticated,(req,res) => {
-  //console.log(req.user);
-  res.send("Welcome User");
+app.get('/',(req,res) => {
+  console.log(req.user)
+  res.redirect('/home');
 })
 
+app.get('/home', (req, res) => {
+  console.log(req.user)
+  res.render('home',{user : req.user});
+});
+
+// app.get("/dashboard",ensureAuthenticated,(req,res) => {
+//   //console.log(req.user);
+//   res.send("Welcome User");
+// })
+
 app.get("/login",(req, res) => {
-  res.send(`
-    <form method="POST" action="/login">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="password" placeholder="Password">
-      <button type="submit">Log in</button>
-    </form>
-  `);
+  const errorMessage = req.flash('error')[0];
+  // console.log(errorMessage)
+  res.render('login',{message : errorMessage});
 });
 
-app.get("/signin", (req, res) => {
-  res.send(`
-        <form method="POST" action="/signin">
-        <input type="text" name="username" placeholder="Username">
-        <input type="password" name="password" placeholder="Password">
-        <button type="submit">Sign in</button>
-        </form>
-    `);
+app.get("/signup", (req, res) => {
+  const errorMessage = req.flash('error')[0];
+  // console.log(errorMessage)
+  res.render('signup',{message : errorMessage});
 });
 
-app.post("/signin", (req, res, next) => {
-  const { username, password } = req.body;
-  const newUser = new User({ username, password });
-  newUser
-    .save()
-    .then((user) => {
-      req.login(user, (err) => {
-        if (err) console.log(err);
-        res.redirect("/dashboard");
-      });
-    })
-    .catch((err) => console.log(err));
-});
+// app.post("/signup",checkUsername, (req, res, next) => {
+//   const { username, password } = req.body;
+//   const newUser = new User({ username, password });
+//   newUser
+//     .save()
+//     .then((user) => {
+//       req.login(user, (err) => {
+//         if (err) console.log(err);
+//         res.redirect("/");
+//       });
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/dashboard',
+// Sign up route using Passport and failureFlash option
+app.post('/signup', passport.authenticate('signup', {
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
+
+app.post('/login', passport.authenticate('login', {
+  successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
 }));
@@ -120,7 +161,7 @@ app.post('/logout', function(req, res, next) {
   req.logout(function(err) {
     if (err) { return next(err); }
     //console.log(req.user);
-    res.redirect('/login');
+    res.redirect('/');
   });
 });
 
