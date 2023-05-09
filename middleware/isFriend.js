36a0a4a152223b0904401b2c12fe97eecc77d399
Sanjoy Majee <sanjoymajee02@gmail.com>
2023-05-09 22:02:49 +0530

    ommiting url

--- middleware/isFriend.js
+++ middleware/isFriend.js
@@ -1,9 +1,4 @@
-const url = require('url');
-
 exports.isFriend = (req,res,next) => {
-    const referer = req.get('Referer');
-    const urlObject = url.parse(referer,true);
-    const urlPath = urlObject.pathname;
     if(req.user){
         const user = req.user;
         for(const friend of user.friends){
@@ -12,7 +7,7 @@ exports.isFriend = (req,res,next) => {
                 return;
             }
         }
-        res.redirect(urlPath);
+        res.redirect('/profile/' + req.params.username);
     }
     else{
         res.redirect('/auth/login');
