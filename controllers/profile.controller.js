df620e6704e5cca82fa3805477402ac089436fb6
Sanjoy Majee <sanjoymajee02@gmail.com>
2023-05-25 20:54:14 +0530

    solved profile bug when getting a profile unauthorized

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -4,7 +4,7 @@ exports.getUserProfile = async (req, res) => {
   const profileUsername = req.params.profileUsername.toLowerCase()
   const profileUser = await User.findOne({ username: profileUsername })
   const user = req.user
-  const isFriend = user.friends.some((friend) => {
+  const isFriend = user && user.friends.some((friend) => {
     return friend.userId.toString() == profileUser._id.toString()
   })
   res.render('profile', { profileUser, user, isFriend })
